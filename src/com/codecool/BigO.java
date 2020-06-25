8aa626316c894c778f69759971f95db43409b8f0
MartonCsizmazia <csmarton93@gmail.com>
2020-06-25 07:44:58 +0200

    printPairs foo implemented

--- src/com/codecool/BigO.java
+++ src/com/codecool/BigO.java
@@ -50,4 +50,16 @@ public class BigO{
 
 
 
+    //COMPLEXITY: (O)n^2    n steps called n times
+    public void printPairs(int[] array){
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length; j++) {
+                System.out.println(array[i] + "," + array[j]);
+            }
+        }
+    }
+
+
+
+
 }
