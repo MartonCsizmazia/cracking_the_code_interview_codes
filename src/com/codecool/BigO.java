37ca9e4761c7f01a579ce09a68b9f29e4ea46338
MartonCsizmazia <csmarton93@gmail.com>
2020-06-25 08:00:04 +0200

    j = i+1 for loop implemented

--- src/com/codecool/BigO.java
+++ src/com/codecool/BigO.java
@@ -60,6 +60,15 @@ public class BigO{
     }
 
 
+    public void printUnorderedPairs(int[] array){
+        for (int i = 0; i < array.length; i++) {
+            for (int j = i+1; j < array.length; j++) {
+                System.out.println(array[i] + "," + array[j]);
+            }
+        }
+    }
+
+
 
 
 }
