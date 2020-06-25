32ca374ff47c326ef48ebfc774811ef02f0c0505
MartonCsizmazia <csmarton93@gmail.com>
2020-06-25 07:12:53 +0200

    pairSumSequence implemented

--- src/com/codecool/BigO.java
+++ src/com/codecool/BigO.java
@@ -1,4 +1,16 @@
 package com.codecool;
 
 public class BigO {
+
+    private int pairSumSequence(int n){
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += pairSum(i, i+1);
+        }
+        return sum;
+    }
+
+    private int pairSum(int a, int b) {
+        return a+b;
+    }
 }
