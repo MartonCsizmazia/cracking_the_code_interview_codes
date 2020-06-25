8d8b19cf86c3b2e87bfcc74a8d9915dbf3481658
MartonCsizmazia <csmarton93@gmail.com>
2020-06-25 07:36:41 +0200

    f tree implemented

--- src/com/codecool/BigO.java
+++ src/com/codecool/BigO.java
@@ -1,8 +1,11 @@
 package com.codecool;
 
-public class BigO {
+public class BigO{
 
-    private int pairSumSequence(int n){
+
+    //COMPLEXITY: (O)1  -> only (O)1  exists any given time
+                           //calls are not called simultaneusly, only 1 at the time
+    public int pairSumSequence(int n){
         int sum = 0;
         for (int i = 0; i < n; i++) {
             sum += pairSum(i, i+1);
@@ -10,8 +13,20 @@ public class BigO {
         return sum;
     }
 
-    private int pairSum(int a, int b) {
+    public int pairSum(int a, int b) {
         return a+b;
+    }
+
+
+
+    //COMPLEXITY: (O)n  -> only (O)n exists any given time
+    public int f(int n){
+        if(n<=1){
+            //System.out.println("return 1");
+            return 1;
+        }
+        //System.out.println("return (n-1)*2");
+        return f(n-1)+f(n-1);
     }
 
 }
--- src/com/codecool/Main.java
+++ src/com/codecool/Main.java
@@ -5,5 +5,8 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         BigO bigO = new BigO();
+        System.out.println(bigO.pairSumSequence(5));
+        System.out.println(bigO.f(4));
+
     }
 }
